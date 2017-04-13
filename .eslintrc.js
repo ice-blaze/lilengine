396372499a4da1a7847050fe96081141f9a07c55
ice-blaze <franketip@gmail.com>
2017-04-13 21:52:58 +0200

    Add eslint
    npm run lint is a script that runs the complete check with eslint
    add a first attempt of the eslint code convention

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+	"rules": {
+		"semi": ["error", "never"],
+		"quotes": ["error", "double"]
+
+
+
+	}
+};
