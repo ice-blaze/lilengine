c0f61d1c71e857bf1c0c48f5feb0df26d90d79f8
ice-blaze <franketip@gmail.com>
2017-04-16 03:52:23 +0200

    Fix code format view.js

--- javascript/view.js
+++ javascript/view.js
@@ -3,13 +3,14 @@ const view = {
 	update_hierarchy() {
 		$("#gameobject_hierarchy>li").remove()
 		let i = 0
-		for (gameobject of gameobject_hierarchy) {
-			$("#gameobject_hierarchy").append("<li><a name='" + i++ + "' id='" + gameobject.name + "'>" + gameobject.name + "</a></li>")
+		gameobject_hierarchy.forEach((gameobject) => {
+			i += 1
+			$("#gameobject_hierarchy").append(`<li><a name='${i}' id='${gameobject.name}'>${gameobject.name}</a></li>`)
 			$("#" + gameobject.name).click((ev) => {
 				selected_game_object = gameobject_hierarchy[ev.currentTarget.name]
 				view.update_inspector()
 			})
-		}
+		})
 	},
 	update_inspector() {
 		$("#gameobject_inspector").empty()
@@ -17,19 +18,19 @@ const view = {
 		const decimal = 4
 		if(sgo) {
 			$("#gameobject_inspector").append(
-				sgo.name + "<br>" +
-				"position: <br>" +
-				"x: " + sgo.position[0].toFixed(decimal) + "<br>" +
-				"y: " + sgo.position[1].toFixed(decimal) + "<br>" +
-				"z: " + sgo.position[2].toFixed(decimal) + "<br>" +
-				"rotation: <br>" +
-				"x: " + sgo.rotate[0].toFixed(decimal) + "<br>" +
-				"y: " + sgo.rotate[1].toFixed(decimal) + "<br>" +
-				"z: " + sgo.rotate[2].toFixed(decimal) + "<br>" +
-				"scale: <br>" +
-				"x: " + sgo.scale[0].toFixed(decimal) + "<br>" +
-				"y: " + sgo.scale[1].toFixed(decimal) + "<br>" +
-				"z: " + sgo.scale[2].toFixed(decimal) + "<br>"
+				`${sgo.name}<br>
+				position: <br>
+				x: ${sgo.position[0].toFixed(decimal)}<br>
+				y: ${sgo.position[1].toFixed(decimal)}<br>
+				z: ${sgo.position[2].toFixed(decimal)}<br>
+				rotation: <br>
+				x: ${sgo.rotate[0].toFixed(decimal)}<br>
+				y: ${sgo.rotate[1].toFixed(decimal)}<br>
+				z: ${sgo.rotate[2].toFixed(decimal)}<br>
+				scale: <br>
+				x: ${sgo.scale[0].toFixed(decimal)}<br>
+				y: ${sgo.scale[1].toFixed(decimal)}<br>
+				z: ${sgo.scale[2].toFixed(decimal)}<br>`
 			)
 		} else {
 			$("#gameobject_inspector").append("None")
@@ -41,22 +42,21 @@ const view = {
 const menu_appearing_time = 500
 
 $(document).ready(() => {
-
 	$("#leftMenuButton").click(() => {
 		$("#leftMenu").toggle("slide", {
-			direction: 'left'
+			direction: "left",
 		}, menu_appearing_time)
 	})
 
 	$("#topMenuButton").click(() => {
 		$("#topMenu").toggle("slide", {
-			direction: 'up'
+			direction: "up",
 		}, menu_appearing_time)
 	})
 
 	$("#rightMenuButton").click(() => {
 		$("#rightMenu").toggle("slide", {
-			direction: 'right'
+			direction: "right",
 		}, menu_appearing_time)
 	})
 
