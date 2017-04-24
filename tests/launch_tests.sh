0df09fd286b56caa91185eb72393ca0d2b160641
ice-blaze <franketip@gmail.com>
2017-04-24 19:36:08 +0200

    Test the UI with selenium/protractor fix#24

--- tests/launch_tests.sh
+++ tests/launch_tests.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
-# TODO add an webdriver update before the start
+
+npm run pack
+
+# check if selenium is installed
+node_modules/.bin/webdriver-manager update
+selePort="4444"
+httpPort="8329"
+
 # start selenium server just for this test run
 (node_modules/.bin/webdriver-manager start &)
 # Wait for port 4444 to be listening connections
-while ! nc -z 127.0.0.1 4444; do sleep 2; done
+while ! nc -z 127.0.0.1 $selePort; do sleep 2; done
 
 # Start the web app
-(node_modules/.bin/http-server . &)
+(node_modules/.bin/http-server -p $httpPort &)
 # Guessing your http-server listen at port 80
-while ! nc -z 127.0.0.1 8080; do sleep 1; done
+while ! nc -z 127.0.0.1 $httpPort; do sleep 1; done
 
 # Finally run protractor
 node_modules/.bin/protractor .protractor.config.js
 
 # Cleanup webdriver-manager and http-server processes
-fuser -k -n tcp 4444
-fuser -k -n tcp 8080
+fuser -k -n tcp $selePort
+fuser -k -n tcp $httpPort
