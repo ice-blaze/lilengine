6fb59680cdccd8b0c6194964e450aac0b7afa70e
Etienne Frank <franketip@gmail.com>
2017-05-18 22:36:31 +0200

    Move createbuffer function to utils

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 import SkyBox from "./skybox"
 import GameObject from "./game_object"
 import initCanvasButton from "./canvas_buttons"
-import { range } from "./utils"
+import { range, createFramebuffer } from "./utils"
 import ChromaticAberration from "./chromatic_aberration"
 
 // GLOBALS
@@ -26,6 +26,7 @@ function getGl(canvas) {
 		return undefined
 	}
 }
+
 
 function main() {
 	const canvas = document.getElementById("demoCanvas")
@@ -60,43 +61,9 @@ function main() {
 	const skybox = new SkyBox(gl, "skybox", canvas)
 	skybox.scale.set([100000, 100000, 100000])
 
-	function createFramebuffer(width, height) {
-		// Framebuffer part
-		const buffer = gl.createFramebuffer()
-		gl.bindFramebuffer(gl.FRAMEBUFFER, buffer)
-		buffer.width = width
-		buffer.height = height
-		const texture = gl.createTexture()
-		gl.bindTexture(gl.TEXTURE_2D, texture)
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-		gl.texImage2D(
-			gl.TEXTURE_2D, 0, gl.RGBA, buffer.width, buffer.height, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-			null,
-		)
-		gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0)
-
-		const render = gl.createRenderbuffer()
-		gl.bindRenderbuffer(gl.RENDERBUFFER, render)
-		gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, width, height)
-		gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0)
-		gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, render)
-
-		gl.bindTexture(gl.TEXTURE_2D, null)
-		gl.bindFramebuffer(gl.FRAMEBUFFER, null)
-		gl.bindRenderbuffer(gl.RENDERBUFFER, null)
-		return {
-			buffer,
-			texture,
-			render,
-		}
-	}
-
 	const chromatic = new ChromaticAberration(gl)
 
-	const bufftex = createFramebuffer(canvas.width, canvas.height)
+	const bufftex = createFramebuffer(gl, canvas.width, canvas.height)
 
 	let timeOld = 0
 	const counterList = []
